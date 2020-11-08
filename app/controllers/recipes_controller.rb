13339127b674da4d66debd0e0b78fa5666d229e7
rokas luksous <tarakonas1@gmx.de>
2020-11-08 17:04:51 +0100

    added controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,29 @@
 class RecipesController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  # def recipe_params
+  #   params.require(:recipe).permit(:name, :preparationTime, :category, :calories, :description, :preparation)
+  # end
 end
