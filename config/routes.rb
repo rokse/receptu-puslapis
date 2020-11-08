aef135601fe65d6b41ef30df017d8904b17044f9
rokas luksous <tarakonas1@gmx.de>
2020-11-08 17:36:09 +0100

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  get 'about', to:'pages#about'
+  get 'contact', to:'pages#contact'
+
+  resources :recipes, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
