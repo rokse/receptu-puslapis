44e242b4a7d732c021a587150e9d077d47978443
rokas luksous <tarakonas1@gmx.de>
2020-11-08 17:44:35 +0100

    added recipe  model

---
+++ db/migrate/20201108163615_create_recipes.rb
@@ -0,0 +1,14 @@
+class CreateRecipes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.integer :preparation_time
+      t.string :category
+      t.integer :calories
+      t.text :description
+      t.text :preparation
+
+      t.timestamps
+    end
+  end
+end
