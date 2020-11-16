19f47ccba13edc6d86406ad4c2ec84f06cfcaad0
rokas luksous <tarakonas1@gmx.de>
2020-11-16 18:24:02 +0100

    devise development

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'about', to:'pages#about'
-  get 'contact', to:'pages#contact'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
   resources :recipes, only: [:index, :show, :new, :create, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
